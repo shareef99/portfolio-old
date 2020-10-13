8dbb0bcda78f6eda10909967ea7e89d3a68408c1
shareef <nadeemshareef934@gmail.com>
2020-10-13 21:53:43 +0530

    Add blog page

--- components/header.jsx
+++ components/header.jsx
@@ -39,8 +39,8 @@ export default function Header() {
                             </Link>
                         </li>
                         <li>
-                            <Link href="/#about">
-                                <a>About</a>
+                            <Link href="/blog">
+                                <a>Blog</a>
                             </Link>
                         </li>
                         <li>
---
+++ pages/blog.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function blog() {
+    return (
+        <div>
+            <h1>Hello from Blog</h1>
+        </div>
+    )
+}
