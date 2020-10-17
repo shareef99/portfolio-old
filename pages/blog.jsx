8e75645dae4ccf1b197369536ae69525bd97a901
shareef <nadeemshareef934@gmail.com>
2020-10-17 21:29:23 +0530

    finish blogs

--- pages/blog.jsx
+++ pages/blog.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
-import Link from 'next/link'
+import React from "react";
+import Link from "next/link";
+import fs from "fs";
 
-export default function blog() {
+export default function blog({ slugs }) {
     return (
         <div>
             <h1>Hello from Blog</h1>
-            <Link href="/test1"><a>Test1</a></Link>
+            <h3>Blogs:</h3>
+            {slugs.map((slug) => {
+                return (
+                    <div key={slug}>
+                        <Link href={"/blog/" + slug}>
+                            <a>{slug}</a>
+                        </Link>
+                    </div>
+                );
+            })}
         </div>
-    )
+    );
 }
+
+export const getStaticProps = async () => {
+    const files = fs.readdirSync("posts");
+
+    return {
+        props: {
+            slugs: files.map((filename) => filename.replace(".md", "")),
+        },
+    };
+};
