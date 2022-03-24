e38268008d0e20cc5893ecaa38a2a057063c2982
shareef <nadeemshareef934@gmail.com>
2022-03-24 16:02:39 +0530

    � redesign home page with tailwind

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import Head from "next/head";
 import Link from "next/link";
-// @ts-ignore
 import Style from "../styles/pages/index.module.scss";
-// @ts-ignore
 import UtilStyles from "../styles/utils.module.scss";
 import useIsInViewport from "use-is-in-viewport";
 import Layout from "../components/layout";
@@ -30,51 +28,55 @@ export default function index() {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Shareef | Home</title>
       </Head>
-      <section>
-        <section className={Style.heroSection}>
-          <div>
-            <h1>I'm Shareef</h1>
-            <p>
-              A <span></span>
-            </p>
-          </div>
-        </section>
-        <section
-          id="introduction"
-          className={`${UtilStyles.containerWithPadding} ${Style.introSection}`}
-        >
-          <h2 className={UtilStyles.h2}>Introduction</h2>
-          <div>
-            <img
-              ref={targetRef}
-              className={`${
-                isInViewport
-                  ? UtilStyles.fadeInFromRight
-                  : UtilStyles.fadeInFromRightFalse
-              }`}
-              src="images/man.webp"
-              alt="profile photo"
-            />
-          </div>
-          <h3 className={UtilStyles.h3}>
-            Hello <span>👋</span>
-          </h3>
+
+      <section
+        className="bg-codingMan bg-no-repeat bg-cover bg-center md:bg-fixed h-[90vh] text-orange 
+          flex-center text-center"
+      >
+        <div>
+          <h1 className="text-[5rem] font-cursive">I'm Shareef</h1>
           <p>
-            I'm shareef, an engineering student, a web developer and a constant
-            learner, while learning I have developed some websites you can visit
-            them in the{" "}
-            <Link href="/project">
-              <a>project</a>
-            </Link>
-            &nbsp;section.
+            A <span className="subtitle"></span>
           </p>
-        </section>
-        <section
-          id="skills"
-          className={`${UtilStyles.containerWithPadding} ${Style.skillSection}`}
-        >
-          <h2 className={UtilStyles.h2}>Skills</h2>
-          <div className={Style.skills}>
+        </div>
+      </section>
+      <section
+        id="introduction"
+        className="container py-24 bg-white text-primary-text-color text-center"
+      >
+        <h2 className="text-left">Introduction</h2>
+        <div>
+          <img
+            ref={targetRef}
+            className={`${
+              isInViewport
+                ? UtilStyles.fadeInFromRight
+                : UtilStyles.fadeInFromRightFalse
+            } mx-auto my-12`}
+            src="images/man.webp"
+            alt="profile photo"
+          />
+        </div>
+        <h3>
+          Hello <span className="wave">👋</span>
+        </h3>
+        <p className="text-left leading-6 md:w-3/4 md:mx-auto">
+          I'm shareef, an engineering student, a web developer and a constant
+          learner, while learning I have developed some websites you can visit
+          them in the{" "}
+          <Link href="/project">
+            <a>project</a>
+          </Link>
+          &nbsp;section.
+        </p>
+      </section>
+      <section
+        id="skills"
+        className={`${Style.skillSection} bg-pink text-primary-text-color`}
+      >
+        <div className="container py-24">
+          <h2>Skills</h2>
+          <div className={`${Style.skills}`}>
             <div className={Style.skillsItem}>
               <div className={Style.imgDiv}>
                 <img
@@ -89,7 +91,7 @@ export default function index() {
                 />
               </div>
               <div>
-                <h3 className={UtilStyles.h3}>Web Development 🧑‍💻</h3>
+                <h3>Web Development 🧑‍💻</h3>
                 <p>
                   I started coding in mid 2k19. I find my passion for Web
                   Development, check out my{" "}
@@ -114,7 +116,7 @@ export default function index() {
                 />
               </div>
               <div>
-                <h3 className={UtilStyles.h3}>Teaching 👨‍🏫</h3>
+                <h3>Teaching 👨‍🏫</h3>
                 <p>
                   Apart from programming, I love to teach. I enjoy teaching
                   Maths. <q>The best way to learn is to teach</q>.
@@ -135,7 +137,7 @@ export default function index() {
                 />
               </div>
               <div>
-                <h3 className={UtilStyles.h3}>leadership 🧑🏻‍🤝‍🧑🏻😊</h3>
+                <h3>leadership 🧑🏻‍🤝‍🧑🏻😊</h3>
                 <p>
                   Leadership is not a rank or position, it is the choice, the
                   responsibility to bring out the best from each and every
@@ -149,48 +151,49 @@ export default function index() {
               </div>
             </div>
           </div>
-        </section>
-        <section id="about" className={`${UtilStyles.containerWithPadding}`}>
-          <h2 className={UtilStyles.h2}>About</h2>
-
-          <div id="about" className={`${Style.aboutSection}`}>
-            <ol start={0}>
-              <li>I am a Frontend developer + Designer.</li>
-              <li>
-                I have earn certificate in Responsive Web Design from{" "}
-                <a
-                  href="http://freecodecamp.org"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  freeCodeCamp
-                </a>{" "}
-                check out my{" "}
-                <a
-                  href="https://www.freecodecamp.org/certification/shareef99/responsive-web-design"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  certificate
-                </a>
-                .
-              </li>
-              <li>
-                I have strong Knowledge of CSS, SCSS(preprocessor) and design
-                principles.
-              </li>
-              <li>
-                I believe in a simple and beautiful design which can attract
-                more and more audience.
-              </li>
-              <li>I'm currently working on React + NextJS.</li>
-              <li>
-                I am willing to upgrade my self and your company with full
-                efforts and curiosity.
-              </li>
-            </ol>
-          </div>
-        </section>
+        </div>
+      </section>
+      <section id="about" className="container py-24">
+        <h2>About</h2>
+        <div id="about" className="text-primary-text-color mt-5 ml-1">
+          <ol start={0}>
+            <li className="listItem">I am a Frontend developer + Designer.</li>
+            <li className="listItem">
+              I have earn certificate in Responsive Web Design from{" "}
+              <a
+                href="http://freecodecamp.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                freeCodeCamp
+              </a>{" "}
+              check out my{" "}
+              <a
+                href="https://www.freecodecamp.org/certification/shareef99/responsive-web-design"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                certificate
+              </a>
+              .
+            </li>
+            <li className="listItem">
+              I have strong Knowledge of CSS, SCSS(preprocessor) and design
+              principles.
+            </li>
+            <li className="listItem">
+              I believe in a simple and beautiful design which can attract more
+              and more audience.
+            </li>
+            <li className="listItem">
+              I'm currently working on React + NextJS.
+            </li>
+            <li className="listItem">
+              I am willing to upgrade my self and your company with full efforts
+              and curiosity.
+            </li>
+          </ol>
+        </div>
       </section>
     </Layout>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,30 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      xs: "475px",
+      ...defaultTheme.screens,
+    },
+    container: {
+      padding: "2rem",
+      center: true,
+    },
+    extend: {
+      backgroundImage: {
+        codingMan: "url('/images/coding man.webp')",
+      },
+      colors: {
+        orange: "var(--orange)",
+        white: "var(--white)",
+        pink: "var(--pink)",
+        "primary-text-color": "var(--primary-text)",
+      },
+    },
   },
   plugins: [],
 };
