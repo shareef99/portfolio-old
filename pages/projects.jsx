5d4ef96f5dbad8142ffa8a8f5175b0d14b8c6d85
shareef <nadeemshareef934@gmail.com>
2022-04-09 20:24:27 +0530

    ✏ added project page

---
+++ pages/projects.jsx
@@ -0,0 +1,52 @@
+import Head from "next/head";
+import Layout from "../components/layout";
+import { projects } from "../public/dummyData";
+
+export default function Project() {
+  return (
+    <Layout>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta
+          name="description"
+          content="see cool websites Shareef coded through out the web development journey."
+        />
+        <meta
+          name="keywords"
+          content="projects, project, shareef, shareef99, shareefBhai, web development projects, html-css projects"
+        />
+
+        <meta name="author" content="Shareef" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Project | Shareef</title>
+        <link
+          rel="shortcut icon"
+          href="images/s-logo.png"
+          type="image/x-icon"
+        />
+      </Head>
+      <section className="container">
+        <h2 className="my-12">Projects</h2>
+        <div className="flex flex-wrap m-auto text-center justify-center">
+          {projects.map((project) => {
+            return (
+              <div
+                key={project.id}
+                className="my-6 mx-12 w-72 max-w-full bg-blue text-xl shadow-xl hover:scale-110 transition-all duration-300 ease-in rounded"
+              >
+                <a href={project.href} target="_blank">
+                  <img
+                    src={project.src}
+                    alt={project.alt}
+                    className="max-w-full w-full rounded"
+                  />
+                  <p className="my-2">{project.title}</p>
+                </a>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    </Layout>
+  );
+}
--- public/dummyData.js
+++ public/dummyData.js
@@ -26,3 +26,76 @@ export const skills = [
       "Leadership usually gravitates to the man who can get up and say what he thinks",
   },
 ];
+
+export const projects = [
+  {
+    id: 1,
+    src: "images/rps.png",
+    href: "https://rock-paper-scissors-shareef.web.app/",
+    alt: "Rock Paper Scissors image",
+    title: "Rock Paper Scissors",
+  },
+  {
+    id: 2,
+    src: "images/simon.png",
+    alt: "Simon Game",
+    href: "https://shareef99.github.io/Simon-Game/",
+    title: "Simon Game",
+  },
+  {
+    id: 3,
+    src: "images/drumKit.png",
+    alt: "Drum Kit Photo",
+    href: "https://shareef99.github.io/Drum-Kit/",
+    title: "Drum-Kit",
+  },
+  {
+    id: 4,
+    src: "images/flyo.png",
+    href: "https://shareef99.github.io/Fylo-dark-theme-landing-page/",
+    title: "Flyo Dark theme",
+    alt: "Flyo Dark theme",
+  },
+  {
+    id: 5,
+    src: "images/rqg.png",
+    alt: "Random Quote Generator",
+    href: "https://quote-generator-9.web.app/",
+    title: "Random Quote Generator",
+  },
+  {
+    id: 6,
+    src: "images/dice.png",
+    alt: "Dice photo",
+    href: "",
+    title: "Dice Website",
+  },
+  {
+    id: 7,
+    src: "images/ticTacToe.png",
+    alt: "tic-tac-toe-photo",
+    href: "https://tic-tac-toe-shareef.web.app/",
+    title: "Tic Tac Toe | Game",
+  },
+  {
+    id: 8,
+    src: "images/tindog.png",
+    alt: "tindog photo",
+    href: "https://shareef99.github.io/tindog/",
+    title: "Tindog Website",
+  },
+  {
+    id: 9,
+    src: "images/itemLister.png",
+    alt: "Item Lister Photo",
+    href: "https://shareef99.github.io/ItemList/",
+    title: "Item Lister",
+  },
+  {
+    id: 10,
+    src: "images/huddle.png",
+    alt: "Huddle Photo",
+    href: "https://shareef99.github.io/Huddle-landing-page-with-alternating-feature-blocks/",
+    title: "Huddle Landing Page",
+  },
+];
