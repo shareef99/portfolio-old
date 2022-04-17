44258e1baa292f4a5bf9ce4147737ce111815ac4
shareef <nadeemshareef934@gmail.com>
2022-04-17 21:25:26 +0530

    � hot add loading symbol to blogs page

--- pages/blogs/index.jsx
+++ pages/blogs/index.jsx
@@ -5,13 +5,14 @@ import Layout from "../../components/layout";
 
 export default function blog() {
   const [posts, setPosts] = useState();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch("https://dev.to/api/articles?username=shareef")
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
         setPosts(data);
+        setLoading(false);
       });
   }, []);
 
@@ -32,30 +33,36 @@ export default function blog() {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta httpEquiv="content-type" content="text/html; charset=UTF-8" />
       </Head>
-      <section className="pt-12 dark:bg-slate-400 dark:text-dark-text-color">
-        <div className="container">
-          <h2>Blogs</h2>
-          <div className="w-11/12 pb-8">
-            {posts &&
-              posts.map(({ id, title, description, published_at, url }) => (
-                <div key={id} id="blogContainer" className="py-8 ml-4">
-                  <h3 className="text-2xl">
-                    <a href={url} target="_blank">
-                      {title}
-                    </a>
-                  </h3>
-                  <p className="text-lg">
-                    {description}{" "}
-                    <a href={url} target="_blank">
-                      ...Learn More Now
-                    </a>
-                  </p>
-                  <p>{published_at.slice(0, 10)}</p>
-                </div>
-              ))}
-          </div>
+      {loading ? (
+        <div className="container h-screen w-screen flex-center">
+          <div className="loading" />
         </div>
-      </section>
+      ) : (
+        <section className="pt-12 dark:bg-slate-400 dark:text-dark-text-color">
+          <div className="container">
+            <h2>Blogs</h2>
+            <div className="w-11/12 pb-8">
+              {posts &&
+                posts.map(({ id, title, description, published_at, url }) => (
+                  <div key={id} id="blogContainer" className="py-8 ml-4">
+                    <h3 className="text-2xl">
+                      <a href={url} target="_blank">
+                        {title}
+                      </a>
+                    </h3>
+                    <p className="text-lg">
+                      {description}{" "}
+                      <a href={url} target="_blank">
+                        Learn More Now
+                      </a>
+                    </p>
+                    <p>{published_at.slice(0, 10)}</p>
+                  </div>
+                ))}
+            </div>
+          </div>
+        </section>
+      )}
     </Layout>
   );
 }
