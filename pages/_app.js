ee65dcd5ed6d8348359db0a13cfbf67008721e46
NadeemShareef <105916625+NadeemShareef@users.noreply.github.com>
2023-11-26 19:34:26 +0530

    Redirect to new portfolio

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
+import { useEffect } from "react";
 import "../styles/global.css";
 import { ThemeProvider } from "next-themes";
+import { useRouter } from "next";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("https://postfolio-shareef.vercel.app");
+  }, []);
+
   return (
     <ThemeProvider defaultTheme="dark" attribute="class">
       <Component {...pageProps} />
